9e6e1d333467ce8ae2f5fe9bc534882849795b5e
bukovyn <bukovyn@gmail.com>
2019-05-27 23:56:55 -0400

    Reformat conditionals

--- bash/f_keys.sh
+++ bash/f_keys.sh
@@ -2,9 +2,8 @@
 # remap 1-5 -> F1-F5 and vice versa
 
 ARGS="[start|stop]"
-if [[ $# == 0 ]]; then
-	echo -e "remap function keys\n# fkeys $ARGS"
-elif [[ $1 == "start" ]]; then
+
+if [[ $1 == "start" ]]; then
 	xmodmap -e "keycode 10 = F1"
 	xmodmap -e "keycode 11 = F2"
 	xmodmap -e "keycode 12 = F3"
@@ -18,6 +17,6 @@ elif [[ $1 == "stop" ]]; then
 	xmodmap -e "keycode 13 = 4 0x024"
 	xmodmap -e "keycode 14 = 5 0x025"
 else
-	echo -e "invalid argument\n# fkeys $ARGS"
+	echo "usage: $(basename $0) $ARGS" >&2
 fi
 
