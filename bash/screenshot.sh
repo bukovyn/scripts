688613ee4c728fb997582a84d224009ff22df930
bukovyn <bukovyn@gmail.com>
2019-06-18 21:12:09 -0400

    Updates

--- bash/screenshot.sh
+++ bash/screenshot.sh
@@ -2,4 +2,4 @@
 # if aliased, the date gets initialized only once when .rc file is loaded
 SCREENSHOTS="$HOME/images/screenshots"
 
-maim -su "$SCREENSHOTS/$(date +%s).jpg"
+maim -sum 10 "$SCREENSHOTS/$(date +%s).jpg"
